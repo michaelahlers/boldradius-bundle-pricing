62a4716cc020fdab182c1520cd74dfec07006970
Michael Ahlers <michael@ahlers.co>
2016-01-08 18:42:21 -0500

    Add handy convenience class inspired by ReactiveMongo's Play module.

---
+++ src/main/scala/boldradius/json/PartialFormat.scala
@@ -0,0 +1,33 @@
+package boldradius.json
+
+import play.api.libs.json.{Format, JsError, JsResult, JsValue}
+
+/**
+ * Makes writing [[play.api.libs.json.Format]] implementations more concise.
+ *
+ * {{{
+ * new PartialFormat[BigDecimal] {
+ *   override def partialReads = {
+ *     case JsNumber(v) => JsSuccess(v)
+ *     case _ => JsError(ValidationError("error.expected.jsstring"))
+ *   }
+ *
+ *   override def partialWrites = {
+ *     case v => JsNumber(v)
+ *   }
+ * }
+ * }}}
+ *
+ * @tparam T See [[play.api.libs.json.Format]].
+ */
+trait PartialFormat[T] extends Format[T] {
+
+  def partialReads: PartialFunction[JsValue, JsResult[T]]
+
+  def partialWrites: PartialFunction[T, JsValue]
+
+  final def writes(t: T): JsValue = partialWrites(t)
+
+  final def reads(json: JsValue) = partialReads.lift(json).getOrElse(JsError( s"""Unsupported JSON value "$json".""""))
+
+}
