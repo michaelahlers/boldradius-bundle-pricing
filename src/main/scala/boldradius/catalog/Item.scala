f7536f60f3682029810f32afa618b0618d4e230e
Michael Ahlers <michael@ahlers.co>
2016-01-08 18:06:17 -0500

    Add JSON reads and writes for catalog items.

--- src/main/scala/boldradius/catalog/Item.scala
+++ src/main/scala/boldradius/catalog/Item.scala
@@ -1,5 +1,8 @@
 package boldradius.catalog
 
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+
 /**
  * Represents an item in the shopping catalog.
  *
@@ -13,3 +16,17 @@ case class Item(
   id: ItemId = ItemId.next,
   SKU: String
 )
+
+object Item {
+
+  implicit def reads: Reads[Item] = (
+    (__ \ 'id).read[Long].map(ItemId(_)) and
+      (__ \ 'SKU).read[String]
+    ) (Item.apply _)
+
+  implicit def writes: OWrites[Item] = (
+    (__ \ 'id).write[Long].contramap[ItemId](_.value) and
+      (__ \ 'SKU).write[String]
+    ) (unlift(Item.unapply))
+
+}
--- src/test/scala/boldradius/catalog/ItemSpec.scala
+++ src/test/scala/boldradius/catalog/ItemSpec.scala
@@ -2,6 +2,8 @@ package boldradius.catalog
 
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json.JsSuccess
+import play.api.libs.json.Json._
 
 class ItemSpec
   extends WordSpec
@@ -12,6 +14,16 @@ class ItemSpec
 
     "have a unique id. value" in {
       Item(SKU = "duplicate") shouldNot be(Item(SKU = "duplicate"))
+    }
+
+  }
+
+  "JSON serializers" should {
+
+    "read and write without loss" in {
+      val expected = Item(SKU = "ABC123")
+      val actual = toJson(expected).validate[Item]
+      actual should be(JsSuccess(expected))
     }
 
   }
