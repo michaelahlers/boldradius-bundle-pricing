a4367666bb80f2e8d78d3556890141be76dd6460
Michael Ahlers <michael@ahlers.co>
2016-01-08 17:45:09 -0500

    Add initial comments.

--- src/main/scala/boldradius/catalog/Item.scala
+++ src/main/scala/boldradius/catalog/Item.scala
@@ -1,5 +1,14 @@
 package boldradius.catalog
 
+/**
+ * Represents an item in the shopping catalog.
+ *
+ * @param SKU Stock keeping unit distinctly identifies an item for sale.
+ *
+ * @author [[mailto:michael@ahlers.co Michael Ahlers]]
+ *
+ * @see [[https://en.wikipedia.org/wiki/Stock_keeping_unit Stock keeping unit]]
+ */
 case class Item(
   id: ItemId = ItemId.next,
   SKU: String
