9a54bf25e808b6a8a5a0f9c584c620cc09e75668
Michael Ahlers <michael@ahlers.co>
2016-01-09 14:59:27 -0500

    Add documentation.

--- src/main/scala/boldradius/scala/collection/TraversableCountByOps.scala
+++ src/main/scala/boldradius/scala/collection/TraversableCountByOps.scala
@@ -2,11 +2,38 @@ package boldradius.scala.collection
 
 import scala.language.higherKinds
 
+/**
+ * Additional operations wrapper.
+ *
+ * {{{
+ * import boldradius.scala.collection.withCountBy
+ *
+ * val chars = List('a', 'b', 'c', 'A', 'B', 'C', 'a', 'b', 'c')
+ * val counts = chars.counted
+ *
+ * assert(chars.counted == Map('a' -> 2, 'b' -> 2, 'c' -> 2, 'A' -> 1, 'B' -> 1, 'C' -> 1))
+ * assert(chars.countBy(_.toLower) == Map('a' -> 3, 'b' -> 3, 'c' -> 3))
+ * }}}
+ *
+ * @see [[withCountBy]]
+ */
 class TraversableCountByOps[A, C[A] <: Traversable[A]](c: C[A]) {
 
+  /**
+   * A short-hand notation of [[scala.collection.Traversable.groupBy grouping]] all elements by a given identity function, and mapping over the values with [[scala.collection.Traversable.size size]].
+   *
+   * @param f Chooses which property of `A` to consider when grouping, which may be an instance of `A` itself.
+   *
+   * @tparam K Resulting key type from `f`.
+   *
+   * @return An index of occurrences by value identity.
+   */
   def countBy[K](f: A => K): Map[K, Int] =
     c.groupBy(f).mapValues(_.size)
 
+  /**
+   * Variation on [[countBy]] which passes the [[scala.Predef.identity identity]] function by default.
+   */
   def counted: Map[A, Int] = countBy(identity)
 
 }
--- src/main/scala/boldradius/scala/collection/package.scala
+++ src/main/scala/boldradius/scala/collection/package.scala
@@ -4,6 +4,9 @@ import scala.language.{higherKinds, implicitConversions}
 
 package object collection {
 
+  /**
+   * Import to augment any [[scala.collection.Traversable]] with [[TraversableCountByOps.countBy]].
+   */
   implicit def withCountBy[A, C[A] <: Traversable[A]](c: C[A]): TraversableCountByOps[A, C] = new TraversableCountByOps(c)
 
 }
