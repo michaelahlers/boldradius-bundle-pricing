7ac554abdae05d9c835ad03d1d2deaeceb545668
Michael Ahlers <michael@ahlers.co>
2016-01-09 14:23:11 -0500

    Remove ad hoc adjusted utility (use Scalaz's MapOps.alter).

--- src/main/scala/boldradius/scala/collection/package.scala
+++ src/main/scala/boldradius/scala/collection/package.scala
@@ -4,7 +4,4 @@ import scala.language.implicitConversions
 
 package object collection {
 
-  /** Import this to provide convenient augmentation of [[scala.collection.Map]] with [[MapAdjustedOps.adjusted]]. */
-  implicit def withAdjusted[K, V](m: Map[K, V]): MapAdjustedOps[K, V] = new MapAdjustedOps(m)
-
 }
