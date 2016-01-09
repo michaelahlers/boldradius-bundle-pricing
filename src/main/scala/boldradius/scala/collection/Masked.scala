6d485b89285bef967d83f6a83f092641748423ef
Michael Ahlers <michael@ahlers.co>
2016-01-09 16:15:05 -0500

    Improve documentation.

--- src/main/scala/boldradius/scala/collection/Masked.scala
+++ src/main/scala/boldradius/scala/collection/Masked.scala
@@ -2,8 +2,19 @@ package boldradius.scala.collection
 
 import scala.language.higherKinds
 
+/**
+ * Represents the outcome of [[TraversableMaskedOps masking]] a [[scala.collection.Traversable]] using a given pattern.
+ *
+ * - Any pattern (empty or not) matched against an empty [[scala.collection.Traversable stream]] is [[MaskedEmpty]].
+ * - Nothing masked in a [[scala.collection.Traversable stream]] is [[MaskedNone]].
+ * - Partial elements masked in a [[scala.collection.Traversable stream]] is [[MaskedSome]].
+ * - All elements masked in a [[scala.collection.Traversable stream]] is [[MaskedAll]].
+ */
 sealed trait Masked[+C]
 
+/**
+ * @see [[Masked]]
+ */
 object Masked {
 
   def apply[A, C[A] <: Traversable[A]](subset: C[A], matches: C[A]): Masked[C[A]] =
@@ -21,12 +32,14 @@ object Masked {
 
 }
 
-/** Any mask (empty or not) applied to an empty traversable. */
+/** No elements were supplied to be masked. */
 case object MaskedEmpty extends Masked[Nothing]
 
-/** */
+/** _No_ elements were masked by the pattern (making this type's value the universal set). */
 case class MaskedNone[+C](universe: C) extends Masked[C]
 
+/** A partial match was achieved (making this type's value a subset, with matching elements provided for convenience). */
 case class MaskedSome[+C](subset: C, matches: C) extends Masked[C]
 
+/** _All_ elements were masked by the pattern (making this type's value the universal set). */
 case class MaskedAll[+C](universe: C) extends Masked[C]
