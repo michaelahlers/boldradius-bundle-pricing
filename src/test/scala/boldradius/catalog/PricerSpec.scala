509140bdadcbb436956a9fc970ad19ec5cb50483
Michael Ahlers <michael@ahlers.co>
2016-01-10 17:01:59 -0500

    Fix incorrect test fixtures.

--- src/test/scala/boldradius/catalog/PricerSpec.scala
+++ src/test/scala/boldradius/catalog/PricerSpec.scala
@@ -2,12 +2,14 @@ package boldradius.catalog
 
 import boldradius.catalog.bundling.Rule
 import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{Matchers, WordSpec}
 import squants.market.USD
 
 trait PricerSpec
   extends WordSpec
           with Matchers
+          with ScalaFutures
           with LazyLogging {
 
   def pricer: Pricer
@@ -29,19 +31,19 @@ trait PricerSpec
     import items.apple
 
     val A = Rule(USD(1.99), apple)
-    val AA = Rule(USD(2.15), apple)
+    val AA = Rule(USD(2.15), apple, apple)
 
     val rules = List(A, AA)
 
     "an apple is bought=" must {
       s"cost ${A.cost}" in {
-        pricer(rules, apple) should be(A.cost)
+        pricer(rules, apple).futureValue should be(A.cost)
       }
     }
 
     "two apples are bought" must {
       s"cost ${AA.cost}" in {
-        pricer(rules, apple, apple) should be(AA.cost)
+        pricer(rules, apple, apple).futureValue should be(AA.cost)
       }
     }
 
@@ -59,31 +61,31 @@ trait PricerSpec
 
     "a loaf is bought alone" must {
       s"cost ${B.cost}" in {
-        pricer(rules, bread) should be(B.cost)
+        pricer(rules, bread).futureValue should be(B.cost)
       }
     }
 
     "a stick of margarine is bought alone" must {
       s"cost ${M.cost}" in {
-        pricer(rules, margarine) should be(M.cost)
+        pricer(rules, margarine).futureValue should be(M.cost)
       }
     }
 
     "a loaf of bread and stick of margarine bought together" must {
       s"cost ${B.cost + M.cost}" in {
-        pricer(rules, bread, margarine) should be(B.cost + M.cost)
+        pricer(rules, bread, margarine).futureValue should be(B.cost + M.cost)
       }
     }
 
     "a loaf of bread and two sticks of margarine bought together" must {
       s"cost ${BMM.cost}" in {
-        pricer(rules, bread, margarine, margarine) should be(BMM.cost)
+        pricer(rules, bread, margarine, margarine).futureValue should be(BMM.cost)
       }
     }
 
     "two loafs of bread and three sticks of margarine bought together" must {
       s"cost ${B.cost + BMM.cost + M.cost}" in {
-        pricer(rules, bread, bread, margarine, margarine, margarine) should be(BMM.cost + M.cost)
+        pricer(rules, bread, bread, margarine, margarine, margarine).futureValue should be(B.cost + BMM.cost + M.cost)
       }
     }
 
