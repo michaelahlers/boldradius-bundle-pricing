8906c3c434b4525441ee1da355971ce788eeeeb6
Michael Ahlers <michael@ahlers.co>
2016-01-10 17:41:19 -0500

    Add a logger.

--- src/test/scala/boldradius/catalog/SimplePricerSpec.scala
+++ src/test/scala/boldradius/catalog/SimplePricerSpec.scala
@@ -1,7 +1,10 @@
 package boldradius.catalog
 
+import com.typesafe.scalalogging.LazyLogging
+
 class SimplePricerSpec
-  extends PricerSpec {
+  extends PricerSpec
+          with LazyLogging {
 
   override def pricer: Pricer = new SimplePricer
 
