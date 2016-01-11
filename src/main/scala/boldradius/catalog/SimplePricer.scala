dba13cd308b2f54cf802a15ba6711992f833f27e
Michael Ahlers <michael@ahlers.co>
2016-01-10 20:42:26 -0500

    Make this easier to reason about.

--- src/main/scala/boldradius/catalog/SimplePricer.scala
+++ src/main/scala/boldradius/catalog/SimplePricer.scala
@@ -13,22 +13,36 @@ class SimplePricer
   extends Pricer
           with LazyLogging {
 
-  private case class Node(rule: Option[Rule], concerns: List[Item])
+  sealed trait Node {
+    def concerns: List[Item]
+  }
 
-  private def materialize(rules: List[Rule], items: List[Item]) = {
+  case class Root(concerns: List[Item]) extends Node
+
+  case class Internal(rule: Rule, concerns: List[Item]) extends Node
+
+  case class Leaf(rule: Rule, concerns: List[Item] = Nil) extends Node
+
+  def materialize(rules: List[Rule], cart: List[Item]) = {
+
     def pathFor(stack: List[(Node, List[Rule])]): List[Rule] =
-      stack.collect({ case (Node(Some(r), _), _) => r })
+      stack collect {
+        case (Internal(r, _), _) => r
+        case (Leaf(r, _), _) => r
+      }
 
     @tailrec
     def build(stack: List[(Node, List[Rule])], results: Set[Map[Rule, Int]]): Set[Map[Rule, Int]] =
       stack match {
 
-        /** Empty stack indicates all combinations have been explored. */
-        case Nil =>
+        /** Arriving at the root indicates all matches have been explored. */
+        case Nil | (Root(_), Nil) :: Nil =>
+
           results
 
         /** No more rules left to evaluate at this position in the path. */
-        case (root, Nil) :: tail if root.concerns.isEmpty =>
+        case (Leaf(rule, Nil), Nil) :: tail =>
+
           val path: List[Rule] = pathFor(stack)
           val result: Map[Rule, Int] = path.foldLeft(Map.empty[Rule, Int]) { case (a, rule) =>
             a.alter(rule)(_.map(_ + 1).orElse(Some(1)))
@@ -36,12 +50,12 @@ class SimplePricer
 
           build(tail, results + result)
 
-        /* TODO: Error handling. */
         case (_, Nil) :: tail =>
           build(tail, results)
 
         /** Evaluate the next in line rule for this node's concerns. */
         case (root, rh :: rt) :: tail =>
+
           val mask = rh.SKUs.counted
           root.concerns.masked(mask, _.SKU) match {
 
@@ -49,25 +63,25 @@ class SimplePricer
             case MaskedEmpty =>
               build((root, Nil) :: tail, results)
 
-            /** This rule was entirely inapplicable. Continue by offering remaining rules back to the queue. */
+            /** This rule was entirely inapplicable. Continue by offering this node with remaining rules back to the queue. */
             case MaskedNone(_) =>
               build((root, rt) :: tail, results)
 
             /** Every concern was masked. Descend further, adding a node for this rule, include it, but don't offer to evaluate any further rules. */
             case MaskedAll(_) =>
-              val child = Node(Some(rh), Nil)
+              val child = Leaf(rh)
               build((child, Nil) ::(root, rt) :: tail, results)
 
             /** A few concerns remain for this node. Descend further, adding a node, include it, and offer to evaluate all rules again for remaining concerns. */
             case MaskedSome(in, _) =>
-              val child = Node(Some(rh), in)
+              val child = Internal(rh, in)
               build((child, rules) ::(root, rt) :: tail, results)
 
           }
 
       }
 
-    build(List(Node(None, items) -> rules), Set.empty)
+    build(List(Root(cart) -> rules), Set.empty)
 
   }
 
