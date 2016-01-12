fd9416c83f3ba2934433347c431120d6cab265f4
Michael Ahlers <michael@ahlers.co>
2016-01-12 00:29:53 -0500

    Add comprehensive documentation.

--- src/main/scala/boldradius/catalog/Pricer.scala
+++ src/main/scala/boldradius/catalog/Pricer.scala
@@ -6,7 +6,37 @@ import squants.market.Money
 import scala.concurrent.Future
 
 /**
- * A (typical) [[Pricer]] implementation exhaustively searches for the lowest price for a shopping cart, given rules which may provide discounts under certain circumstances.
+ * A (typical) [[Pricer]] implementation searches for the lowest price for a shopping cart, given rules which may provide discounts under certain combinations of items.
+ *
+ * == Sample Problem ==
+ *
+ * The order in which rules are evaluated against a cart can affect the outcome (''e.g.'', by eliminating potential bulk discounts). Consider a scenario with rules `{ {A}, {B}, {A,A}, {A,B} }` for cart `{ a, a, b }`.
+ *
+ * These paths all the way those rules may be applied:
+  1. `{ {A}, {A}, {B} }`
+  1. `{ {A}, {A,B} }`
+  1. `{ {B}, {A}, {A} }`
+  1. `{ {B}, {A,A} }`
+  1. `{ {A,A}, {B} }`
+  1. `{ {A,B}, {A} }`
+ *
+ * Spurious paths removed:
+  1. `{ {A}, {A}, {B} }`
+  1. `{ {A}, {A,B} }`
+  1. `{ {B}, {A,A} }`
+ *
+ * Finally, where `{A}` is \$3.00, `{B}` is \$2.00, `{A,A}` is \$5.00, `{A,B}` is \$3.00, total costs for each respective combination are:
+  1. \$3.00 + \$3.00 + \$2.00 = \$8.00
+  1. \$3.00 + \$3.00 = \$6.00
+  1. \$2.00 + \$5.00 = \$7.00
+ *
+ * == Performance Notes ==
+ *
+ * Implementations here '''are not performant'''. They are good enough for homework. Optimizations applying rules in order of decreasing complexity are pending.
+ *
+ * A comprehensively more sophisticated approach involves using a [[http://en.wikipedia.org/wiki/Constraint_programming constraint solver]] (''e.g.'', [[http://choco-solver.org Choco]], [[http://bach.istc.kobe-u.ac.jp/copris Copris]]) or [[http://wikipedia.org/wiki/Simulated_annealing simulated annealing]].
+ *
+ * @see [[http://wikipedia.org/wiki/Knapsack_problem Knapsack problem]] ([[http://wikipedia.org Wikipedia]])
  */
 trait Pricer {
 
