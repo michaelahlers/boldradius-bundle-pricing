a4a7e5fd1cfee184f1e0b7222d428b20786fb1a8
Michael Ahlers <michael@ahlers.co>
2016-01-11 20:54:00 -0500

    Remove futures and use a stateful aggregator actor.

--- src/main/scala/boldradius/catalog/AkkaPricer.scala
+++ src/main/scala/boldradius/catalog/AkkaPricer.scala
@@ -1,15 +1,15 @@
 package boldradius.catalog
 
 import akka.actor._
-import akka.pattern.{ask, pipe}
+import akka.pattern.ask
 import akka.util.Timeout
-import boldradius.catalog.AkkaPricer.{Frontend, Result, Solve}
-import boldradius.catalog.Pricer.UnmatchedItemsException
+import boldradius.catalog.AkkaPricer.{Result, Solve, Solver}
 import boldradius.catalog.bundling.Rule
 import boldradius.scala.collection._
 import com.typesafe.scalalogging.LazyLogging
 import squants.Money
 
+import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -20,95 +20,127 @@ class AkkaPricer(context: ActorRefFactory)
 
   import context.dispatcher
 
-  val frontend = context.actorOf(Props(new Frontend))
+  val solver = context.actorOf(Props(new Solver))
 
   implicit def timeout = Timeout(5 seconds)
 
   override def apply(rules: List[Rule], items: List[Item]): Future[Money] =
-    (frontend ? Solve(rules, items)).mapTo[Result] map {
-      case result if result.matches == items.toSet =>
-        result.costs.min
-      case result =>
-        throw new UnmatchedItemsException(rules, items.toSet.diff(result.matches))
+    (solver ? Solve(rules, items)).mapTo[Result] map { result =>
+      result.costs.min
     }
+
+  //(frontend ? Solve(rules, items)).mapTo[Result] map {
+  //  case result if result.matches == items.toSet =>
+  //    result.costs.min
+  //  case result =>
+  //    throw new UnmatchedItemsException(rules, items.toSet.diff(result.matches))
+  //}
+
 }
 
 object AkkaPricer
   extends LazyLogging {
 
-  case class Solution(path: List[Rule]) {
+  case class Path(rules: List[Rule]) {
 
     def cost: Money =
-      path
+      rules
         .counted
         .map({ case (rule, count) => rule.cost * count })
         .reduce(_ + _)
 
-  }
+    def ++:(foo: Traversable[Rule]) = copy(rules = foo ++: rules)
 
-  case class Solve(rules: List[Rule], items: List[Item], path: List[Rule] = Nil, matches: Set[Item] = Set.empty)
-
-  case class Result(solutions: List[Solution], matches: Set[Item]) {
-
-    def costs: List[Money] = solutions.map(_.cost)
+    def nonEmpty: Boolean = rules.nonEmpty
 
   }
 
-  class Frontend
+  object Path {
+    def apply(rule: Rule): Path = Path(rule :: Nil)
+  }
+
+  case class Solve(rules: List[Rule], items: List[Item], aggregator: Option[ActorRef])
+
+  object Solve {
+
+    def apply(rules: List[Rule], items: List[Item], aggregator: ActorRef): Solve =
+      Solve(rules, items, Some(aggregator))
+
+    def apply(rules: List[Rule], items: List[Item]): Solve =
+      Solve(rules, items, None)
+
+  }
+
+  case class Result(paths: List[Path]) {
+
+    def costs = paths.map(_.cost)
+
+  }
+
+  class Aggregator(expectedRules: List[Rule], head: Option[Rule], replyTo: ActorRef)
     extends Actor {
 
-    import context.dispatcher
+    val paths: mutable.Buffer[Path] = mutable.Buffer.empty
 
-    implicit def timeout = Timeout(5 seconds)
-
-    val solver = context.actorOf(Props(new Backend(self)), "solver")
+    val rules: mutable.Buffer[Rule] = mutable.Buffer.empty
 
     override def receive: Receive = {
 
-      case solve: Solve =>
-        (solver ? solve).mapTo[Result] pipeTo sender
+      case path: Path =>
+        paths += path
+
+      case rule: Rule =>
+        rules += rule
+
+        if (expectedRules.size == rules.size) {
+          replyTo ! Result(paths.filter(_.nonEmpty).map(head ++: _).toList)
+          head.foreach(replyTo !)
+        }
+
+      case result: Result =>
+        paths ++= result.paths
 
     }
 
   }
 
-  class Backend(frontend: ActorRef)
+  object Aggregator {
+
+    def props(rules: List[Rule], rule: Rule, replyTo: ActorRef): Props =
+      Props(new Aggregator(rules, Some(rule), replyTo))
+
+    def props(rules: List[Rule], replyTo: ActorRef): Props =
+      Props(new Aggregator(rules, None, replyTo))
+
+  }
+
+  class Solver
     extends Actor {
-
-    import context.dispatcher
-
-    implicit def timeout = Timeout(5 seconds)
 
     override def receive: Receive = {
 
-      case Solve(rules, items, path, matches) =>
+      case Solve(rules, items, None) =>
+        self ! Solve(rules, items, Some(context.actorOf(Aggregator.props(rules, sender))))
 
-        val results: List[Future[Result]] =
-          rules map { rule =>
+      case Solve(rules, items, Some(aggregator)) =>
+        rules foreach { rule =>
+          val mask = rule.SKUs.counted
 
-            val mask = rule.SKUs.counted
-            items.masked(mask, _.SKU) match {
+          items.masked(mask, _.SKU) match {
 
-              case MaskedAll(out) =>
-                Future(Result(Solution(path :+ rule) :: Nil, matches ++ out))
+            case MaskedAll(_) =>
+              aggregator ! Path(rule)
+              aggregator ! rule
 
-              case MaskedSome(in, out) =>
-                (frontend ? Solve(rules, in, path :+ rule, matches ++ out)).mapTo[Result]
+            case MaskedSome(in, _) =>
+              self ! Solve(rules, in, context.actorOf(Aggregator.props(rules, rule, aggregator)))
 
-              case _ =>
-                Future(Result(Nil, matches))
+            case _ =>
+              aggregator ! Path(Nil)
+              aggregator ! rule
 
-            }
           }
-
-        val result =
-          for {
-            r <- Future.sequence(results)
-            solutions = r.flatMap(_.solutions)
-            matches = r.flatMap(_.matches).toSet
-          } yield Result(solutions, matches)
-
-        result pipeTo sender
+        }
 
     }
 
