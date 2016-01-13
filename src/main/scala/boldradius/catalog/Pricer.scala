38fdb205cd6169e25d2c1b1c7b10e8d22a0d0dee
Michael Ahlers <michael@ahlers.co>
2016-01-13 15:07:46 -0500

    Add usage notes.

--- src/main/scala/boldradius/catalog/Pricer.scala
+++ src/main/scala/boldradius/catalog/Pricer.scala
@@ -8,11 +8,58 @@ import scala.concurrent.{ExecutionContext, Future}
 /**
  * A (typical) [[Pricer]] implementation searches for the lowest price for a shopping cart, given rules which may provide discounts under certain combinations of items.
  *
+ * == Usage Notes ==
+ *
+ * Simply define [[Item items]], [[Rule rules]], and pick a [[Pricer pricer]]. This API is unopinionated about how rules are defined, but it deliberately avoids fractional price arithmetic.
+ *
+ * See the following example:
+ *
+ * {{{
+import boldradius.catalog._
+import squants.market.USD
+
+import scala.concurrent.Await.result
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+/* Define inventory items. */
+
+object Bread extends Item(SKU = "BREAD")
+
+object Margarine extends Item(SKU = "MARGARINE")
+
+/* Define how they are priced. */
+
+val B = Rule(USD(3.00), Bread)
+val M = Rule(USD(2.50), Margarine)
+
+/** Two for the price of one discount. */
+val BMM = Rule(B.cost + M.cost, Bread, Margarine, Margarine)
+
+val rules = List(B, M, BMM)
+
+val pricer: Pricer = new ChocoPricer
+
+/** Customer buys one loaf of bread. */
+assert(result(pricer.apply(rules, Cart(Bread)), 1 second).cost == USD(3.00))
+
+/** Or buys one stick of margarine. */
+assert(result(pricer.apply(rules, Cart(Margarine)), 1 second).cost == USD(2.50))
+
+/** Or buys bread and margarine together. */
+assert(result(pricer.apply(rules, Cart(Bread, Margarine)), 1 second).cost == USD(5.50))
+
+/** But may receive a free stick. */
+assert(result(pricer.apply(rules, Cart(Bread, Margarine, Margarine)), 1 second).cost == USD(5.50))
+ * }}}
+ *
+ * More complete examples may be found in the respective unit test.
+ *
  * == Sample Problem ==
  *
  * The order in which rules are evaluated against a cart can affect the outcome (''e.g.'', by eliminating potential bulk discounts). Consider a scenario with rules `{ {A}, {B}, {A,A}, {A,B} }` for cart `{ a, a, b }`.
  *
- * These paths all the way those rules may be applied:
+ * Those rules may be applied as:
   1. `{ {A}, {A}, {B} }`
   1. `{ {A}, {A,B} }`
   1. `{ {B}, {A}, {A} }`
@@ -20,7 +67,7 @@ import scala.concurrent.{ExecutionContext, Future}
   1. `{ {A,A}, {B} }`
   1. `{ {A,B}, {A} }`
  *
- * Spurious paths removed:
+ * With spurious paths removed:
   1. `{ {A}, {A}, {B} }`
   1. `{ {A}, {A,B} }`
   1. `{ {B}, {A,A} }`
@@ -32,9 +79,7 @@ import scala.concurrent.{ExecutionContext, Future}
  *
  * == Performance Notes ==
  *
- * Implementations here '''are not performant'''. They are good enough for homework. Optimizations applying rules in order of decreasing complexity are pending.
- *
- * A comprehensively more sophisticated approach involves using a [[http://en.wikipedia.org/wiki/Constraint_programming constraint solver]] (''e.g.'', [[http://choco-solver.org Choco]], [[http://bach.istc.kobe-u.ac.jp/copris Copris]]) or [[http://wikipedia.org/wiki/Simulated_annealing simulated annealing]].
+ * Implementations here '''are not performant'''. They are good enough for homework. A useful exercise entails employing a [[http://en.wikipedia.org/wiki/Constraint_programming constraint solver]] (''e.g.'', [[http://choco-solver.org Choco]], [[http://bach.istc.kobe-u.ac.jp/copris Copris]]).
  *
  * @see [[http://wikipedia.org/wiki/Knapsack_problem Knapsack problem]] ([[http://wikipedia.org Wikipedia]])
  */
@@ -61,7 +106,7 @@ object Pricer {
         .format(
         items.map(_.SKU).mkString("{", ", ", "}")
         // ,rules.map(_.SKUs.mkString("{", ", ", "}")).mkString("{", ", ", "}")
-        )
+      )
   }
 
 }
