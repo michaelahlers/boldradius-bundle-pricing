6a333ad5094b433fd9ef12d0ec4984390ad9b44b
Michael Ahlers <michael@ahlers.co>
2016-01-13 13:41:17 -0500

    Clean up a bit.

--- src/test/scala/boldradius/catalog/ChocoPricerSpec.scala
+++ src/test/scala/boldradius/catalog/ChocoPricerSpec.scala
@@ -2,10 +2,7 @@ package boldradius.catalog
 
 import com.typesafe.scalalogging.LazyLogging
 
-//import squants.market.USD
-
-//import scala.concurrent.Await
-//import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class ChocoPricerSpec
   extends PricerSpec
@@ -13,19 +10,17 @@ class ChocoPricerSpec
 
   override def pricer: Pricer = new ChocoPricer
 
-  //  "Solve before the heat death of the universe" when {
+  //"Solve before the heat death of the universe" when {
   //
-  //    def awaitPricer(rules: List[Rule], items: List[Item], timeout: Duration = 60 seconds) =
-  //      Await.result(pricer(rules, items), timeout)
+  //  import squants.market.USD
   //
-  //    "worst-case scenario (all items, all rules)" in {
-  //      val items = List.fill(2)(Inventory.all).flatten
-  //       val items = List.fill(100)(Apple)
-  //      val rules = Rules.all
-  //      awaitPricer(rules, items) should be(USD(27.00))
-  //    }
-  //
+  //  "worst-case scenario (all items, all rules)" in {
+  //    val items = List.fill(20)(Inventory.all).flatten
+  //    val rules = Rules.all
+  //    pricer(rules, items).futureValue should be(USD(27.00))
   //  }
+  //
+  //}
 
   "Matcher" must {
 
