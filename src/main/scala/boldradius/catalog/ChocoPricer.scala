a767e3e70ba68833b01997fa765088f495f8f6d4
Michael Ahlers <michael@ahlers.co>
2016-01-13 13:27:37 -0500

    Add a utility that'll help set proper constraints on the solver.

--- src/main/scala/boldradius/catalog/ChocoPricer.scala
+++ src/main/scala/boldradius/catalog/ChocoPricer.scala
@@ -1,9 +1,13 @@
 package boldradius.catalog
 
+import boldradius.scala.collection.{MaskedAll, MaskedSome}
+import boldradius.syntax.scala.collection._
 import com.typesafe.scalalogging.LazyLogging
 import squants.market.Money
 
+import scala.annotation.tailrec
 import scala.concurrent.Future
+import scalaz.Scalaz._
 
 class ChocoPricer
   extends Pricer
@@ -14,5 +18,34 @@ class ChocoPricer
 }
 
 object ChocoPricer {
+
+  def matcher(rules: List[Rule], items: List[Item]): (Set[Item], Map[Rule, Int]) = {
+
+    @tailrec
+    def counter(queue: List[(Rule, List[Item])], remainders: Set[Item], occurrences: Map[Rule, Int]): (Set[Item], Map[Rule, Int]) =
+      queue match {
+
+        case Nil =>
+          remainders -> occurrences
+
+        case (r, is) :: tail =>
+          is.masked(r.SKUs.counted, _.SKU) match {
+
+            case MaskedAll(out) =>
+              counter(tail, remainders -- out, occurrences |+| Map(r -> 1))
+
+            case MaskedSome(in, out) =>
+              counter((r -> in) +: tail, remainders -- out, occurrences |+| Map(r -> 1))
+
+            case _ =>
+              counter(tail, remainders, occurrences)
+
+          }
+
+      }
+
+    counter(rules.map(_ -> items), items.toSet, Map.empty)
+
+  }
 
 }
--- src/test/scala/boldradius/catalog/ChocoPricerSpec.scala
+++ src/test/scala/boldradius/catalog/ChocoPricerSpec.scala
@@ -27,4 +27,37 @@ class ChocoPricerSpec
   //
   //  }
 
+  "Matcher" must {
+
+    "identify rule occurrences among items" in {
+      val items = List(Apple, Apple, Apple, Apple, Bread, Bread, Bread)
+      val rules = List(A, B, AA, AB, BB, AAA)
+
+      val expected =
+        Map(
+          A -> 4,
+          B -> 3,
+          AA -> 2,
+          AB -> 3,
+          BB -> 1,
+          AAA -> 1
+        )
+
+      val (_, actual) = ChocoPricer.matcher(rules, items)
+
+      actual should be(expected)
+    }
+
+    "identify non-matching items" in {
+      val items = List(Apple, Daikon, Apple, Apple, Apple, Bread, Bread, Bread, Celery)
+      val rules = List(A, B, AA, AB, BB, AAA)
+
+      val expected = Set(Celery, Daikon)
+      val (actual, _) = ChocoPricer.matcher(rules, items)
+
+      actual should be(expected)
+    }
+
+  }
+
 }
