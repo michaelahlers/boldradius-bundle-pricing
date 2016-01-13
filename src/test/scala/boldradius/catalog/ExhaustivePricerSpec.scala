e30389288835ec4be833df824b21b32612f2acfc
Michael Ahlers <michael@ahlers.co>
2016-01-13 14:16:55 -0500

    Clean up.

--- src/test/scala/boldradius/catalog/ExhaustivePricerSpec.scala
+++ src/test/scala/boldradius/catalog/ExhaustivePricerSpec.scala
@@ -2,29 +2,22 @@ package boldradius.catalog
 
 import com.typesafe.scalalogging.LazyLogging
 
-//import squants.market.USD
-
-//import scala.concurrent.Await
-//import scala.concurrent.duration._
-
 class ExhaustivePricerSpec
   extends PricerSpec
           with LazyLogging {
 
   override def pricer: Pricer = new ExhaustivePricer
 
-  "Solve before the heat death of the universe" when {
-
-    //def awaitPricer(rules: List[Rule], items: List[Item], timeout: Duration = 60 seconds) =
-    //  Await.result(pricer(rules, items), timeout)
-    //
-    //"worst-case scenario (all items, all rules)" in {
-    //  val items = List.fill(2)(Inventory.all).flatten
-    //  // val items = List.fill(100)(Apple)
-    //  val rules = Rules.all
-    //  awaitPricer(rules, items) should be(USD(27.00))
-    //}
-
-  }
+  //"Solve before the heat death of the universe" when {
+  //
+  //  import squants.market.USD
+  //
+  //  "worst-case scenario (all items, all rules)" in {
+  //    val items = List.fill(20)(Inventory.all).flatten
+  //    val rules = Rules.all
+  //    pricer(rules, items).futureValue should be(USD(27.00))
+  //  }
+  //
+  //}
 
 }
